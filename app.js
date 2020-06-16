79772a5096fc68ec1be62143f4fc276418388858
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-06-16 15:23:13 +0100

    creates a mongodb connection for users and event storage

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 
 const app = express();
 
-app.use(bodyParser.json());
-
 const port = process.env.PORT || 3000;
 
-const events = [];
+app.use(bodyParser.json());
 
 app.use ("/graphql", graphqlHTTP({
     schema: buildSchema(`
@@ -42,25 +42,41 @@ app.use ("/graphql", graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
-            return events
+        events: async () => {
+            try {
+                const events = await Event.find();
+                
+                return events.map((event) => {
+                    console.log({...event._doc });
+                    return { ...event._doc };
+                });
+
+            } catch (err) { throw err; };
         },
-        createEvent: (args) => {
-            event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date
+        createEvent: async (args) => {
+            try {
+                const event = new Event({
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: args.eventInput.price,
+                    date: new Date(args.eventInput.date)
+                });
+
+                const result = await event.save();
+                console.log(result);
+                return { ...result._doc };
+
+            } catch (err) {
+                console.log(err);
+                throw err;
             }
 
-            events.push(event);
-            return event;
+            // return; event;
         }
     },
     graphiql: true
 }))
 
-app.listen(port, () => console.log("server is listening on port " + port));
-
-
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-naofv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`)
+.then(() => app.listen(port, () => console.log("server is listening on port " + port)))
+.catch((err) => console.log(err));
